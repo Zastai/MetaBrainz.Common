2e09dfc873330c6ea1425b153ca31bda634bfde2
Tim Van Holder <tim.vanholder@gmail.com>
2022-01-27 18:32:36 +0100

    Add `AsyncUtils` class
    This currently provides only a `ResultOf` method to synchronously await
    a task.

---
+++ MetaBrainz.Common/AsyncUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+namespace MetaBrainz.Common;
+
+/// <summary>Utility methods related to <see langword="async"/> processing.</summary>
+[PublicAPI]
+public static class AsyncUtils {
+
+  /// <summary>Synchronously awaits a task.</summary>
+  /// <param name="task">The task to await.</param>
+  public static void ResultOf(Task task) => task.ConfigureAwait(false).GetAwaiter().GetResult();
+
+  /// <summary>Synchronously awaits a task.</summary>
+  /// <param name="task">The task to await.</param>
+  /// <typeparam name="T">The return type for the task.</typeparam>
+  /// <returns>The value returned by the task when it completes.</returns>
+  public static T ResultOf<T>(Task<T> task) => task.ConfigureAwait(false).GetAwaiter().GetResult();
+
+}
